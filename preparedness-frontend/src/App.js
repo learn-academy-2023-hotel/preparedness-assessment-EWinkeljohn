20214476c96ede0511cd4c059e4fe08d1c22f783
EWinkeljohn <emcwink@gmail.com>
2023-12-10 20:01:20 -0800

    Front-End Functionality

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,20 +1,32 @@
-import React from "react"
+import React, {useState} from "react"
 import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
+  const [userInput, setUserInput] = useState("")
+  const handleChange = (e) => {
+    // .target.value to extract user input
+    setUserInput(e.target.value)
+  }
+  const reset = (e) => {
+    setUserInput("")
+  }
+    // State variable for modal being open or close. Beginning state false (closed)
+    const [modal, setModal] = useState(false)
+    // Toggle funciton updates modal state to opposite of starting value when invoked
+    const toggle = (props) => setModal(!modal)
   return (
     <div className="entire-content">
       <h1>Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
           <Label for="name">Enter your name</Label>
-          <Input />
+          <Input type="text" value={userInput} onChange={handleChange} />
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
+        <Button onClick={toggle}>Click Me</Button>
+        <Button onClick={reset}>Reset</Button>
+        <ModalComponent isOpen={modal} name={userInput} toggle={toggle}/>
       </div>
     </div>
   )
--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -1,7 +1,27 @@
-import React from "react"
+import React, {useState} from "react";
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 
-const ModalComponent = () => {
-  return <></>
+const ModalComponent = (props) => {
+  // State variable for modal being open or close. Beginning state false (closed)
+  const [modal, setModal] = useState(false)
+  // Toggle funciton updates modal state to opposite of starting value when invoked
+  // Function with a nested button component containing an event handler. HTML type "button" to avoid default form submission with button component
+  const closeButton = (
+    <button className="close" onClick={props.toggle} type="button">
+      x
+    </button>
+  )
+  return (
+  <>
+  {/*Props isOpen = to state of modal. Toggle props passed to communicate with "Click Me" button on App.js. {...props} to get all information from props   */}
+  <Modal isOpen={modal} toggle={props.toggle} {...props}>
+    <ModalHeader close={closeButton}>Hey there {props.name}! </ModalHeader>
+    <ModalBody>
+    Grimalkin siamese kitten and thai jaguar for jaguar panther. Grimalkin maine coon maine coon scottish fold but balinese birman but siberian. American bobtail manx. Kitten. Manx american shorthair manx lynx, bombay. Norwegian forest grimalkin and tomcat tabby and american shorthair, siamese for burmese. Jaguar ragdoll himalayan. Ragdoll. Lion. Savannah american shorthair yet sphynx burmese so lion. Tabby turkish angora. Tomcat singapura. Havana brown tomcat yet munchkin, yet norwegian forest ocicat sphynx birman. Abyssinian panther or cougar for himalayan persian. Manx egyptian mau yet scottish fold or bombay, norwegian forest. Lion sphynx turkish angora, tomcat, so tabby so siamese. Tiger ocicat, siamese. Ragdoll maine coon and abyssinian . Burmese bombay. Bobcat sphynx yet munchkin for panther.
+    </ModalBody>
+  </Modal>
+  </>
+  ) 
 }
 
 export default ModalComponent
